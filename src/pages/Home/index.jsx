eb11b117475d31a16e0db1f914cc6780c7eea59f
msm1379 <mohammadmarandi89@gmail.com>
2023-06-13 22:41:44 +0330

    create banner component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../../components/Header';
 import Slider from '../../components/Slider';
+import Banner from '../../components/Banner';
 
 function Home() {
   return (
     <div>
+      <Banner />
       <Header />
       <Slider />
     </div>
