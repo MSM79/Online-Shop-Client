94a91cba820488a01ec41517289e190cb2ce4f1c
msm1379 <mohammadmarandi89@gmail.com>
2023-06-13 22:35:54 +0330

    create slider

---
+++ src/components/Slider/index.jsx
@@ -0,0 +1,133 @@
+import React from 'react';
+import './style.css';
+
+// import pic1 from '../../images/1.jpg';
+// import pic2 from '../../images/2.jpg';
+// import pic3 from '../../images/3.png';
+// import pic4 from '../../images/4.webp';
+
+const images = [
+  'https://images.unsplash.com/photo-1506501139174-099022df5260?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80',
+  'https://images.unsplash.com/photo-1523438097201-512ae7d59c44?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80',
+  'https://images.unsplash.com/photo-1513026705753-bc3fffca8bf4?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80',
+];
+const Carousel = () => {
+  // We will start by storing the index of the current image in the state.
+  const [currentImage, setCurrentImage] = React.useState(0);
+
+  // We are using react ref to 'tag' each of the images. Below will create an array of
+  // objects with numbered keys. We will use those numbers (i) later to access a ref of a
+  // specific image in this array.
+  const refs = images.reduce((acc, val, i) => {
+    acc[i] = React.createRef();
+    return acc;
+  }, {});
+
+  const scrollToImage = (i) => {
+    // First let's set the index of the image we want to see next
+    setCurrentImage(i);
+    // Now, this is where the magic happens. We 'tagged' each one of the images with a ref,
+    // we can then use built-in scrollIntoView API to do eaxactly what it says on the box - scroll it into
+    // your current view! To do so we pass an index of the image, which is then use to identify our current
+    // image's ref in 'refs' array above.
+    refs[i].current.scrollIntoView({
+      //     Defines the transition animation.
+      behavior: 'smooth',
+      //      Defines vertical alignment.
+      block: 'nearest',
+      //      Defines horizontal alignment.
+      inline: 'start',
+    });
+  };
+
+  // Some validation for checking the array length could be added if needed
+  const totalImages = images.length;
+
+  // Below functions will assure that after last image we'll scroll back to the start,
+  // or another way round - first to last in previousImage method.
+  const nextImage = () => {
+    if (currentImage >= totalImages - 1) {
+      scrollToImage(0);
+    } else {
+      scrollToImage(currentImage + 1);
+    }
+  };
+
+  const previousImage = () => {
+    if (currentImage === 0) {
+      scrollToImage(totalImages - 1);
+    } else {
+      scrollToImage(currentImage - 1);
+    }
+  };
+
+  // Tailwind styles. Most importantly notice position absolute, this will sit relative to the carousel's outer div.
+  const arrowStyle =
+    'absolute flex text-white text-2xl z-10 h-10 w-10 rounded-full opacity-75 flex items-center justify-center';
+
+  // Let's create dynamic buttons. It can be either left or right. Using
+  // isLeft boolean we can determine which side we'll be rendering our button
+  // as well as change its position and content.
+  const sliderControl = (isLeft) => (
+    <button
+      type="button"
+      onClick={isLeft ? previousImage : nextImage}
+      className={`${arrowStyle} ${isLeft ? 'left-2' : 'right-2'}`}
+      style={{ top: '40%' }}
+    >
+      <span role="img" aria-label={`Arrow ${isLeft ? 'left' : 'right'}`}>
+        {isLeft ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            class="w-5 h-5"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            class="w-5 h-5"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        )}
+      </span>
+    </button>
+  );
+
+  return (
+    <div className="w-screen flex justify-center">
+      <div className="w-screen">
+        <div className="relative w-full">
+          <div className="carousel">
+            {sliderControl(true)}
+            {images.map((img, i) => (
+              <div
+                className="w-full flex-shrink-0  h-[400px]"
+                key={img}
+                ref={refs[i]}
+              >
+                <img src={img} className="w-full object-contain" />
+              </div>
+            ))}
+            {sliderControl()}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
