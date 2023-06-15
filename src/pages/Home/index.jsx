652d036cde0da997f9f68cf4b1b3f36e14871fa0
msm1379 <mohammadmarandi89@gmail.com>
2023-06-16 00:17:37 +0330

    Create Products component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import Header from '../../components/Header';
 import Slider from '../../components/Slider';
 import Banner from '../../components/Banner';
+import Products from '../../components/Products';
+import Footer from '../../components/Footer';
+import NewsLetter from '../../components/NewsLetter';
 
 function Home() {
   return (
     <div>
       <Header />
       <Slider />
+      <Products />
+      <NewsLetter />
+      <Footer />
     </div>
   );
 }
