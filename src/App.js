24dfbc1fe2325902577516c7058e0900d357ec23
msm1379 <mohammadmarandi89@gmail.com>
2023-06-20 18:28:03 +0330

    create about us page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Signin from './pages/signin';
 import Signup from './pages/signup';
 import NotFound from './pages/NotFound';
 import ProductsList from './pages/ProductsList';
+import ContactUs from './pages/ContactUs';
+import AboutUs from './pages/AboutUs';
 import './app.css';
 
 export default function App() {
@@ -15,6 +17,8 @@ export default function App() {
         <Route path="signin" element={<Signin />} />
         <Route path="signup" element={<Signup />} />
         <Route path="products" element={<ProductsList />} />
+        <Route path="contactus" element={<ContactUs />} />
+        <Route path="aboutus" element={<AboutUs />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
