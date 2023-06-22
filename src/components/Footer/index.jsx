5c15db85f87117aaf884fd723b932961dd939ce4
msm1379 <mohammadmarandi89@gmail.com>
2023-06-23 01:25:43 +0330

    fix footer links

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer class="p-4 bg-white sm:p-6 ">
+    <footer class="p-4 bg-white sm:p-6 shadow">
       <div class="mx-auto max-w-screen-xl">
         <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
-            <a href="https://flowbite.com" class="flex items-center">
+            <a href="/" class="flex items-center">
               <img
                 src="/images/logo.svg"
                 class="mr-3 h-8"
@@ -20,17 +20,17 @@ export default function Footer() {
           <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
               <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">
-                Resources
+                Procucts
               </h2>
               <ul class="text-gray-600 dark:text-gray-400">
                 <li class="mb-4">
                   <a href="https://flowbite.com" class="hover:underline">
-                    Flowbite
+                    Men
                   </a>
                 </li>
                 <li>
                   <a href="https://tailwindcss.com/" class="hover:underline">
-                    Tailwind CSS
+                    Women
                   </a>
                 </li>
               </ul>
@@ -41,19 +41,13 @@ export default function Footer() {
               </h2>
               <ul class="text-gray-600 ">
                 <li class="mb-4">
-                  <a
-                    href="https://github.com/themesberg/flowbite"
-                    class="hover:underline "
-                  >
-                    Github
+                  <a href="/contactus" class="hover:underline ">
+                    Contact Us
                   </a>
                 </li>
                 <li>
-                  <a
-                    href="https://discord.gg/4eeurUVvTy"
-                    class="hover:underline"
-                  >
-                    Discord
+                  <a href="/aboutus" class="hover:underline">
+                    About US
                   </a>
                 </li>
               </ul>
