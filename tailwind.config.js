f3255a36f26c23b9aee03b6f8caed18014e051c7
msm1379 <mohammadmarandi89@gmail.com>
2023-06-24 20:51:10 +0330

    update screens config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   darkMode: 'class',
   content: ['./src/**/*.{html,jsx}'],
   theme: {
+    screens: {
+      xs: '380px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
   },
   plugins: [],
