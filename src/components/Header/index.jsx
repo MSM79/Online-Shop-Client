0c9476d5b86dd1613011509d93581d20efc8f66b
msm1379 <mohammadmarandi89@gmail.com>
2023-06-24 20:51:19 +0330

    add avatar to header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,6 @@
 import { Fragment, useState } from 'react';
-import { Dialog, Popover, Tab, Transition } from '@headlessui/react';
+import { Dialog, Popover, Tab, Transition, Menu } from '@headlessui/react';
+
 import ShoppingCart from '../ShoppingCart';
 import {
   Bars3Icon,
@@ -7,6 +8,16 @@ import {
   ShoppingBagIcon,
   XMarkIcon,
 } from '@heroicons/react/24/outline';
+
+const user = {
+  name: 'Mohammad Seyfi Marandi',
+  email: 'Mohammadmarandi89@gmail.com',
+  imageUrl: '/images/mohammad.jpg',
+};
+const userNavigation = [
+  { name: 'Your Profile', href: '/profile' },
+  { name: 'Sign out', href: '#' },
+];
 
 const navigation = {
   categories: [
@@ -458,13 +469,45 @@ export default function Header() {
 
                 {/* Search */}
                 <div className="flex lg:ml-6">
-                  <a href="#" className="p-2 text-gray-400 hover:text-gray-500">
-                    <span className="sr-only">Search</span>
-                    <MagnifyingGlassIcon
-                      className="h-6 w-6"
-                      aria-hidden="true"
-                    />
-                  </a>
+                  <Menu as="div" className="relative ml-3">
+                    <div>
+                      <Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <span className="sr-only">Open user menu</span>
+                        <img
+                          className="h-8 w-8 rounded-full"
+                          src={user.imageUrl}
+                          alt=""
+                        />
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        {userNavigation.map((item) => (
+                          <Menu.Item key={item.name}>
+                            {({ active }) => (
+                              <a
+                                href={item.href}
+                                className={classNames(
+                                  active ? 'bg-gray-100' : '',
+                                  'block px-4 py-2 text-sm text-gray-700',
+                                )}
+                              >
+                                {item.name}
+                              </a>
+                            )}
+                          </Menu.Item>
+                        ))}
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
                 </div>
 
                 {/* Cart */}
