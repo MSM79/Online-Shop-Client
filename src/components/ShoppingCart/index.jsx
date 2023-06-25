4c456fdf9c8b64219d5d7fbc1628e07c0055467b
msm1379 <mohammadmarandi89@gmail.com>
2023-06-25 03:44:45 +0330

    fixed shopping card

--- src/components/ShoppingCart/index.jsx
+++ src/components/ShoppingCart/index.jsx
@@ -5,25 +5,23 @@ import { XMarkIcon } from '@heroicons/react/24/outline';
 const products = [
   {
     id: 1,
-    name: 'Throwback Hip Bag',
+    name: 'Artwork Tee',
     href: '#',
     color: 'Salmon',
-    price: '$90.00',
+    price: '$36.00',
     quantity: 1,
-    imageSrc:
-      'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
+    imageSrc: '/images/31.jpg',
     imageAlt:
       'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
   },
   {
     id: 2,
-    name: 'Medium Stuff Satchel',
+    name: 'Basic Tee',
     href: '#',
     color: 'Blue',
-    price: '$32.00',
+    price: '$36.00',
     quantity: 1,
-    imageSrc:
-      'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
+    imageSrc: '/images/24.jpg',
     imageAlt:
       'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
   },
